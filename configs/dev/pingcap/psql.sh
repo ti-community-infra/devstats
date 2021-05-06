52a96aa39bbba78a929ca8890f71e9e36dd61dce
Mini256 <minianter@foxmail.com>
2021-05-06 13:56:13 +0800

    feat: add contributor profile dashboard (#25)

--- configs/dev/pingcap/psql.sh
+++ configs/dev/pingcap/psql.sh
@@ -15,7 +15,7 @@ GHA2DB_PROJECT=pingcap PG_DB=pingcap GHA2DB_LOCAL=1 structure 2>>errors.txt | te
 ./devel/db.sh psql pingcap -c "create extension if not exists pgcrypto" || exit 1
 
 # Notice: the project start time here is not real.
-GHA2DB_PROJECT=pingcap PG_DB=pingcap GHA2DB_LOCAL=1 gha2db 2021-01-01 0 today now "pingcap/tidb,pingcap/dm,pingcap/br,pingcap/tidb-dashboard,pingcap/parser,pingap/ticdc,pingcap/go-ycsb,pingcap/docs-cn,pingcap/docs,pingcap/tidb-binlog,pingcap/blog,pingcap/dumpling,pingcap/tidb-operator,pingcap/tispark,pingcap/docs-tidb-operator,pingcap/docs-dm,pingcap/community,pingcap/kvproto,pingcap/tiup,pingcap/website-docs,pingcap/tidb-tools,pingcap/talent-plan,pingcap/awesome-database-learning,pingcap/failpoint,pingcap/tidb-ansible,pingcap/tidb-docker-compose,pingcap/presentations,pingcap/weekly" 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=pingcap PG_DB=pingcap GHA2DB_LOCAL=1 gha2db 2021-01-01 0 today now pingcap 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=pingcap PG_DB=pingcap GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=pingcap PG_DB=pingcap ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=pingcap PG_DB=pingcap ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
---
+++ configs/dev/test/test_tags.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+GHA2DB_PROJECT=tikv GHA2DB_TAGS_YAML=test/data/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=tikv PG_USER=postgres tags
