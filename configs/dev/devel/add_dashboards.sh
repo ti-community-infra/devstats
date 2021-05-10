f7b7cde5ad26df5ac8dc8fcc1a5c36066b6e59fe
Mini256 <minianter@foxmail.com>
2021-05-10 16:45:51 +0800

    feat: add add dashboard/dashboards tool

--- configs/dev/devel/add_dashboards.sh
+++ configs/dev/devel/add_dashboards.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ -z "$1" ]
-then
-  echo "$0: required dashboard file name"
-  exit 1
-fi
 if [ -z "$FROM_PROJ" ]
 then
   echo "$0: you need to set FROM_PROJ=projname $*"
@@ -43,8 +38,8 @@ do
   to_fullname=`getFullname ${to_proj}`
 
   echo "Copy dashboard config file from project $from_fullname to project $to_fullname"
-  cp "./grafana/dashboards/${from_proj}/${1}" "./grafana/dashboards/${to_proj}/"
+  cp ./grafana/dashboards/${from_proj}/*.json "grafana/dashboards/${to_proj}/"
 
-  FROM="${from_proj}" TO="${to_proj}" FILES=`find ./grafana/dashboards/$to_proj/ -iname "$1"` MODE=ss0 ./devel/mass_replace.sh
-  FROM="${from_fullname}" TO="${to_fullname}" FILES=`find ./grafana/dashboards/$to_proj/ -iname "$1"` MODE=ss0 ./devel/mass_replace.sh
+  FROM="${from_proj}" TO="${to_proj}" FILES="./grafana/dashboards/${to_proj}/*.json" MODE=ss0 ./devel/mass_replace.sh
+  FROM="${from_fullname}" TO="${to_fullname}" FILES="./grafana/dashboards/${to_proj}/*.json" MODE=ss0 ./devel/mass_replace.sh
 done
