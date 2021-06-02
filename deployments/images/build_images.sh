923c23266a67619ccce7458458a09633b890ae88
Mini256 <minianter@foxmail.com>
2021-06-02 17:30:43 +0800

    fix: refine pr opned/closed/merged metrics and dashbaords (#60)
    I forgot to copy the metrics to prod and delete the redundant dashboard in the last PR.

--- deployments/images/build_images.sh
+++ deployments/images/build_images.sh
@@ -147,8 +147,9 @@ then
   cd "$DEV_CONFIG_DIR" || exit 61
 
   tar rf "$DEV_CONFIG_TAR" pingcap tikv chaosmesh metrics partials scripts devel docs projects.yaml
-  tar rf "$DEV_GRAFANA_CONFIG_TAR" grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/dashboards/*/*.json
+  tar rf "$DEV_GRAFANA_CONFIG_TAR" grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/*/grafana_start.sh grafana/dashboards/*/*.json
   tar rf "$DEV_GRAFANA_CONFIG_TAR" grafana/*/custom_sqlite.sql
+  tar rf "$DEV_GRAFANA_CONFIG_TAR" grafana/shared/*
   tar cf "$DEV_API_CONFIG_TAR" projects.yaml
 fi
 
@@ -159,8 +160,9 @@ then
   cd "$PROD_CONFIG_DIR" || exit 62
 
   tar rf "$PROD_CONFIG_TAR" pingcap tikv chaosmesh metrics partials scripts devel docs projects.yaml
-  tar rf "$PROD_GRAFANA_CONFIG_TAR" grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/dashboards/*/*.json
+  tar rf "$PROD_GRAFANA_CONFIG_TAR" grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/*/grafana_start.sh grafana/dashboards/*/*.json
   tar rf "$PROD_GRAFANA_CONFIG_TAR" grafana/*/custom_sqlite.sql
+  tar rf "$PROD_GRAFANA_CONFIG_TAR" grafana/shared/*
   tar cf "$PROD_API_CONFIG_TAR" projects.yaml
 fi
 
