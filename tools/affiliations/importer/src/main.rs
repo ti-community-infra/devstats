3a03a6e459093835862852f2617cd14057627c1c
hi-rustin <rustin.liu@gmail.com>
2021-06-04 22:31:59 +0800

    feat: process the github_users.json file

--- tools/affiliations/importer/src/main.rs
+++ tools/affiliations/importer/src/main.rs
@@ -10,12 +10,16 @@ extern crate dotenv_codegen;
 #[macro_use]
 extern crate log;
 
-use crawler::crawler::{Crawl, Crawler};
 use std::collections::HashSet;
-use std::iter::FromIterator;
 
-static UNKNOWN_AFFILIATION: [&str; 4] = ["", "-", "(Unknown)", "NotFound"];
+use crawler::crawler::{Crawl, Crawler};
 
+/// Invalid affiliations.
+static INVALID_AFFILIATIONS: [&str; 4] = ["", "-", "(Unknown)", "NotFound"];
+/// PingCAP affiliation.
+static PINGCAP_AFFILIATION: &str = "PingCAP";
+
+/// GitHub user info.
 #[derive(Debug, Serialize, Deserialize)]
 struct GitHubUser {
     login: String,
@@ -31,74 +35,134 @@ struct GitHubUser {
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    info!("Start downloading the github_users.json file...");
+
+    info!("Start downloading the github_users file...");
     let resp = reqwest::get("https://github.com/cncf/gitdm/raw/master/src/github_users.json")
         .await
-        .expect("Failed to download the github_users.json file.")
+        .expect("Failed to download the github_users file.")
         .text()
         .await
-        .expect("Failed to convert github_users.json to users text.");
-    info!("Downloading the GitHub_users.json file was successful!");
-    let records: Vec<GitHubUser> =
-        serde_json::from_str(resp.as_str()).expect("Failed to parse github_users.json.");
-    info!("Gets {} users data.", records.len());
+        .expect("Failed to convert github_users to text.");
+    info!("Downloading the github_users file was successful!");
+
+    let mut records: Vec<GitHubUser> =
+        serde_json::from_str(resp.as_str()).expect("Failed to parse github_users file.");
+    info!("{} records were obtained.", records.len());
+
+    info!("Start fetching the PingCAP GitHub logins...");
     let crawler =
         &Crawler::new(dotenv!("APP_ID"), dotenv!("APP_SECRET")).expect("Failed init the crawler.");
     let logins = crawler
         .list_github_logins()
         .await
-        .expect("Failed to get logins.");
-    let mut logins: HashSet<&String> = HashSet::from_iter(logins.iter());
+        .expect("Failed to list logins.");
+    let mut logins: HashSet<&String> = logins.iter().collect::<HashSet<_>>();
+    info!("Fetching the logins to succeed!");
 
-    for mut record in records {
+    for record in &mut records {
         if logins.contains(&record.login) {
-            add_pingcap_affiliation(&mut record);
+            set_pingcap_affiliation(record);
             logins.remove(&record.login);
         }
     }
 
-    println!("Hvae {:#?}", logins.len())
+    logins.iter().for_each(|l| {
+        info!("Add new record for {}.", l);
+        records.push(GitHubUser {
+            login: (*l).clone(),
+            email: l.as_str().to_owned() + "!users.noreply.github.com",
+            affiliation: Some(PINGCAP_AFFILIATION.to_string()),
+            source: Some("manual".to_string()),
+            name: None,
+            commits: 0,
+            location: None,
+            country_id: None,
+        })
+    });
 }
 
-fn add_pingcap_affiliation(user: &mut GitHubUser) {
+/// Set PingCAP affiliation to user.
+fn set_pingcap_affiliation(user: &mut GitHubUser) {
     match &user.affiliation {
+        // If none, set directly to PingCAP.
         None => {
             user.affiliation = {
-                info!("Set {} affiliation to pingcap", user.login,);
-                Some("PingCAP".to_string())
+                info!("Set [{}] affiliation to PingCAP.", user.login);
+                Some(PINGCAP_AFFILIATION.to_string())
             }
         }
         Some(affiliation) => {
-            let new_affiliation = get_pingcap_affiliation(affiliation);
-            info!(
-                "Set {} affiliation from {} to {}",
-                user.login, affiliation, new_affiliation
-            );
-            user.affiliation = Some(new_affiliation);
+            // If it is an invalid affiliation, set directly to PingCAP.
+            if INVALID_AFFILIATIONS.contains(&affiliation.as_str()) {
+                info!(
+                    "Set [{}] invalid affiliation `{}` to PingCAP.",
+                    user.login, affiliation
+                );
+                user.affiliation = Some(PINGCAP_AFFILIATION.to_string());
+            } else {
+                let (new_affiliation, changed) = generate_affiliation_with_pingcap(affiliation);
+                if changed {
+                    info!(
+                        "Set [{}] affiliation from `{}` to `{}`.",
+                        user.login, affiliation, new_affiliation
+                    );
+                    user.affiliation = Some(new_affiliation);
+                }
+            }
         }
     }
 }
 
-fn get_pingcap_affiliation(affiliation: &String) -> String {
-    let pingcap = "PingCAP".to_string();
+/// Generate new affiliation with PingCAP.
+fn generate_affiliation_with_pingcap(affiliation: &str) -> (String, bool) {
+    let pingcap = PINGCAP_AFFILIATION.to_string();
+    let company_separator = ", ";
 
-    if UNKNOWN_AFFILIATION.contains(&affiliation.as_str()) {
-        return pingcap.clone();
-    }
-    return match affiliation.split(", ").collect::<Vec<&str>>().as_slice() {
-        [.., second_to_last, last] => {
-            if *last == "PingCAP" {
-                return affiliation.clone()
+    // The original affiliation look like: "PerkinElmer < 2014-08-01, Independent < 2015-10-01, PwC < 2020-01-01, Simplebet".
+    return match affiliation
+        .split(company_separator)
+        .collect::<Vec<&str>>()
+        .as_slice()
+    {
+        // Get the last two companies.
+        // "PerkinElmer < 2014-08-01, Independent < 2015-10-01, PwC < 2020-01-01, Simplebet" ->
+        // [.., "PwC < 2020-01-01", "Simplebet" ]
+        [.., penultimate, last] => {
+            // If the last company is PingCAP then no change is required.
+            if *last == PINGCAP_AFFILIATION {
+                return (affiliation.to_string(), false);
             }
-            match second_to_last.split(" < ").collect::<Vec<&str>>().first() {
-                None => panic!("shouldn't happen"),
+            // Set the last company record date to the penultimate company date
+            // (Equivalent to ignoring the last company, we can't know exactly when he joined, so we have to do this),
+            // then add the pingcap.
+            match penultimate.split(" < ").collect::<Vec<&str>>().first() {
+                None => panic!("Invalid penultimate affiliation: {}.", penultimate),
                 Some(company) => {
-                    let new_record = second_to_last.replace(company, last);
-                    let new_record = affiliation.replace(last, new_record.as_str());
-                    new_record.as_str().to_owned() + ", " + pingcap.as_str()
+                    // Temp: PwC < 2020-01-01 -> Simplebet < 2020-01-01
+                    let new_record = penultimate.replace(company, last);
+
+                    // Simplebet -> Simplebet < 2020-01-01
+                    let mut new_affiliation = affiliation.to_string();
+                    new_affiliation
+                        .replace_range(affiliation.len() - last.len().., new_record.as_str());
+
+                    // Finally: "PerkinElmer < 2014-08-01, Independent < 2015-10-01, PwC < 2020-01-01, Simplebet < 2020-01-01, PingCAP"
+                    (
+                        new_affiliation.as_str().to_owned() + company_separator + pingcap.as_str(),
+                        true,
+                    )
                 }
             }
         }
-        _ => pingcap,
+        // In other cases where there is only one, replace it directly with PingCAP.
+        [last] => {
+            // If the last company is PingCAP then no change is required.
+            return if *last == PINGCAP_AFFILIATION {
+                (last.to_string(), false)
+            } else {
+                (pingcap, true)
+            };
+        }
+        [] => (pingcap, true),
     };
 }
