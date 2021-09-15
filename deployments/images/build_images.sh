03f68935e9d264dfe2b5b40cccf6fa377f5c6357
Mini256 <minianter@foxmail.com>
2021-09-15 08:29:20 +0800

    fix: add skip date

--- deployments/images/build_images.sh
+++ deployments/images/build_images.sh
@@ -136,7 +136,7 @@ then
   rm -f "$DEVSTATS_SHARED_CONFIG_TAR" 2>/dev/null
 fi
 
-tar cf "$DEVSTATS_SHARED_CONFIG_TAR" grafana jsons countries.csv organizations.yaml
+tar cf "$DEVSTATS_SHARED_CONFIG_TAR" ./*
 
 # Package the devstats config file for dev environment.
 # Notice: This file will override the config files from shared directory and cncf/devstats repository.
